e97c3d0afb00900c45ca617a43aa184313d422b7
parmeet007@gmail.com <parmeet007@gmail.com>
2023-08-01 20:50:31 +0000

    xxx

--- notebooks/Users/parmeet007@gmail.com/D2.py
+++
@@ -1,6 +0,0 @@
-# Databricks notebook source
-print('abcd')
-
-# COMMAND ----------
-
-print('x' * 7)
--- notebooks/Users/parmeet007@gmail.com/Git_Integratino.py
+++
@@ -1,7 +0,0 @@
-# Databricks notebook source
-print('hello')
-
-# COMMAND ----------
-
-a3 = "a" * 3
-print(a3)
